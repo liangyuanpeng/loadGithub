7707e504072a1c5c96488be83801388d11be5e98
fish <gcslyp@gmail.com>
2018-09-29 23:20:47 +0800

    update travis

---
+++ generateGQL.py
@@ -0,0 +1,45 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.types import Type, Field, list_of
+from sgqlc.types.relay import Connection, connection_args
+from sgqlc.operation import Operation
+
+class Follower(Type):
+    login = str
+    avatar_url = str
+    isSiteAdmin = bool
+    email = str
+    updatedAt = str
+    name = str
+    company = str
+
+class FollowingConnection(Connection):
+    nodes = list_of(Follower)
+
+class FollowersConnection(Connection):
+    nodes = list_of(Follower)
+
+class User(Type):
+    following = Field(FollowingConnection, args=connection_args())
+    followers = Field(FollowersConnection, args=connection_args())
+
+class Viewer(Type):
+    login = str
+    avatar_url = str
+    isSiteAdmin = bool
+    email = str
+    updatedAt = str
+    name = str
+    company = str
+
+op = Operation(Query)
+viewer = op.viewer()
+viewer.login()
+viewer.isSiteAdmin()
+viewer.email()
+viewer.name()
+viewer.updatedAt()
+viewer.company()
+
+user = op.user(login=currentUser)
+
+print(op)

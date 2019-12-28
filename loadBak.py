816d586b3eab5eb53486651d0568c6eb11cc1db2
lyp-home <liangyuanpengem@163.com>
2019-12-28 12:42:43 +0800

    add config for worker

--- loadBak.py
+++ loadBak.py
@@ -13,7 +13,6 @@ import redis
 
 import configparser
 
-import os
 
 class Follower(Type):
     login = str
@@ -60,8 +59,7 @@ def insertData(coll,currentUser,data):
         tmp = {
             "name": tmpStr,
             "email": followerOne["email"],
-            "company": followerOne["company"],
-            "login":followerOne["login"]
+            "company": followerOne["company"]
         }
         followingCollList.append(tmp)
 
@@ -121,21 +119,18 @@ def insertListToUser(coll,list):
     for index,user in enumerate(list):
 
         # if user["login"] in userCache:
-        # user在redis cache中
         if redisclient.get(user["login"]) != None:
             print("######################### {0} in userCache".format(user["login"]))
             continue
 
         result = coll.find_one({'login': user["login"]})
         # print("-----------------------------------mongo.login:{0}".format(result))
-        #mongodb没有这个user
         if result == None:
             user["followingNP"] = True
             user["followerNP"] = True
             user["followingEndCursor"] = ''
             user["followerEndCursor"] = ''
             # realyList.append(user)
-            #将user对象写到redis list中
             redisclient.sadd("toInsertUser",json.dumps(user))
         # userCache[user["login"]] = 1
         # redisclient.set(user["login"],1)
@@ -167,21 +162,15 @@ def saveOrUpdateProgress(coll,followingNP,followerNP,currentUser, followingEndCu
                 result["order"] = 0
         coll.update(condition, result)
 
-
-client = pymongo.MongoClient (host = 'mongo' , port = 27017)
+client = pymongo.MongoClient (host = '192.168.1.11' , port = 25017)
 loadGithubDb = client["github1"]
 usersColl = loadGithubDb["users1"]
 followerColl = loadGithubDb["follower1"]
 followingColl = loadGithubDb["following1"]
 taskQueue = queue.Queue(16)
 
-redisclient = redis.Redis(host='redis',port=6379,db=0)
+redisclient = redis.Redis(host='192.168.1.11',port=5379,db=0)
 # redisclient = redis.Redis(host='172.17.0.1',port=6379,db=0)
-
-worker=False
-viewer=False
-loader=False
-saver=False
 
 
 # 方式一
@@ -196,134 +185,35 @@ class Settings(object):
                 setattr(self, k, v)
 
 
-def  initConfig():
-    
-    global redisclient
-    global client
-    
-    global loadGithubDb
-    global usersColl
-    global followerColl
-    global followingColl
-    
-    global viewer
-    global worker
-    global loader
-    global saver
-    
-    env_dist = os.environ 
-    
+def initConfig():
     config = configparser.ConfigParser()
     config.read("conf/config.ini",encoding='utf-8')
-    # settings = Settings("conf/config.ini")
-    
-    if config.has_option("task","viewer"):
-        if config.get("task","viewer")!="":
-            viewer = config.getboolean("task","viewer")
-    
-    if config.has_option("task","worker"):
-        if config.get("task","worker")!="":
-            worker = config.getboolean("task","worker")
-        
-    if config.has_option("task","loader"):
-        if config.get("task","loader")!="":
-            loader = config.getboolean("task","loader")
-            
-    if config.has_option("task","saver"):
-        if config.get("task","saver")!="":
-            saver = config.getboolean("task","saver")    
-            
-    if env_dist.get('LG_VIEWER')!=None:
-        viewer=env_dist.get('LG_VIEWER')
-    if env_dist.get('LG_LOADER')!=None:
-        loader=env_dist.get('LG_LOADER')
-    if env_dist.get('LG_SAVER')!=None:
-        saver=env_dist.get('LG_SAVER')
-    if env_dist.get('LG_WORKER')!=None:
-        worker=env_dist.get('LG_WORKER')
-            
-    redisHost = ""
-    redisPort = 0
-    
-    mongoHost = ""
-    mongoPort = 0
-    
+    settings = Settings("conf/config.ini")
+    print(settings.cf.has_option("db","db_name"))
     if config.has_option("redis","redis_host"):
         if config.has_option("redis","redis_port"):
-            # print("set redis|{0}|{1}".format(config.get("redis","redis_host"),config.getint("redis","redis_port")))
-            if config.get("redis","redis_host")!="":
-                redisHost = config.get("redis","redis_host")
-                redisPort = 6379
-                if config.get("redis","redis_port")!="":
-                    redisPort = config.getint("redis","redis_port")
-               
-   
-
-    if env_dist.get('LG_REDIS_HOST')!=None:
-        redisHost = env_dist.get('LG_REDIS_HOST')
-        redisPort = 6379
-        
-    if env_dist.get('LG_REDIS_PORT')!=None:
-        redisPort =env_dist.get('LG_REDIS_PORT')
-    
-    if redisHost!=None:
-        redisclient = redis.Redis(host=redisHost, port=redisPort, db=0)
+            redisclient = redis.Redis(host=config.get("redis","redis_host"), port=config.getint("redis","redis_port"), db=0)
 
     if config.has_option("mongo","mongo_host"):
         if config.has_option("mongo","mongo_host"):
-            # print("set pymongo|{0}|{1}".format(config.get("mongo","mongo_host"),config.getint("mongo","mongo_port")))
-            if config.get("mongo","mongo_host")!="":
-                mongoHost = config.get("mongo","mongo_host")
-                mongoPort = 27017
-                if config.get("mongo","mongo_port")!="":
-                    mongoPort = config.getint("mongo","mongo_port")
-                
-                
-                
-    if env_dist.get('LG_MONGO_HOST')!=None:
-        mongoHost = env_dist.get('LG_MONGO_HOST')
-        mongoPort = 27017
-        
-    if env_dist.get('LG_MONGO_PORT')!=None:
-        mongoPort =env_dist.get('LG_MONGO_PORT')
-    
-    if mongoHost!=None:
-        client = pymongo.MongoClient(host=mongoHost, port=mongoPort)
-        loadGithubDb = client["github1"]
-        usersColl = loadGithubDb["users1"]
-        followerColl = loadGithubDb["follower1"]
-        followingColl = loadGithubDb["following1"]
-    
+            client = pymongo.MongoClient(host=config.get("mongo","mongo_host"), port=config.getint("mongo","mongo_port"))
+
 
 def main():
-    
-    # client = pymongo.MongoClient (host = 'mongo' , port = 27017)
+
     initConfig()
-    
-    # env_dist = os.environ 
 
     # Call the endpoint:
-    
     url = 'https://api.github.com/graphql'
     headers = {'Authorization': 'bearer 9d58bb126d105413eb4153f4a9d659cfec033983'}
     endpoint = HTTPEndpoint(url, headers,3)
-    
-    # print("====={0}|{1}|{2}|{3}|".format(env_dist.get('LG_VIEWER'),env_dist.get('LG_LOADER'),env_dist.get('LG_SAVER'),env_dist.get('LG_WORKER')))
-    
-    if viewer:
-        taskQueue.put("liangyuanpeng")
-        beginReq("liangyuanpeng",True,endpoint,'','')
-        progressCondition = {"followingNP":True}
-        print(progressCondition)
-    if saver:
-        _thread.start_new(startInsertUser,())
-    # 从mongodb读取数据放入到worker中
-    if loader:
-        _thread.start_new(loadTaskToRedis,())
-    if worker:
-        _thread.start_new(worker,())
+    taskQueue.put("liangyuanpeng")
+    beginReq("liangyuanpeng",True,endpoint,'','')
+    progressCondition = {"followingNP":True}
+    print(progressCondition)
+    # _thread.start_new(startInsertUser,())
+    # _thread.start_new(loadTaskToRedis,())
 
-    #worker
     # while True:
     #     time.sleep(1)
     #     task = redisclient.spop("loadTask")
@@ -355,7 +245,6 @@ def startInsertUser():
                 else:
                     print("#################,startInsertUser mongo exist,srem.user:{0}".format(jsondata["login"]))
                     # usersColl.delete_one({"login":jsondata["login"]})
-                    #数据库已经有这个user了,从todo list从移除 并且加入到user_ cache 表示不进行相关任务
                     redisclient.srem("toInsertUser",u)
                     redisclient.setex("user_" + jsondata["login"], 1, random.randint(60,60*60)+60*60*24*7)
             else:
@@ -370,33 +259,16 @@ def loadTaskToRedis():
         time.sleep(30 + random.randint(1, 30))
         print("----------------------loadTaskToRedis  begin")
         randomInt = random.randint(1, 100)
-        #从mongodb中随机抽取有关注数或又被关注数量的user， order为随机序号
         loadTaskCondition = {"$or": [{"followingNP": True}, {"followerNP": True}], "order": {"$lte": randomInt}}
-        #从mongo查出user todo lise
         resultList = usersColl.find(loadTaskCondition)
         print("===================== loadTaskToRedis begin loadTaskCondition :{0}".format(loadTaskCondition))
         for u in resultList:
-            #7天 随机1小时
             redisclient.setex("user_" + u["login"], 1, random.randint(60, 60 * 60) + 60 * 60 * 24 * 7)
-            #如果user没有在task内,则加入  
             if(redisclient.get("task_"+u["login"])) == None:
                 print("----------------------loadTaskToRedis sadd:{0}".format(u["login"]))
                 u["_id"] = ''
                 redisclient.sadd("loadTask",json.dumps(u))
-                #user加入到task中，3-10分钟内不再执行该user的任务
                 redisclient.setex("task_" + u["login"], u["login"], random.randint(3, 10) * 60)
-
-def worker(endpoint):
-    while True:
-        time.sleep(1)
-        task = redisclient.spop("loadTask")
-        if task != None:
-            print("----------------------worker  begin")
-            print(task)
-            one = json.loads(task)
-            taskQueue.put(one["login"])
-            _thread.start_new_thread(beginReq,(one["login"], False, endpoint, one["followingEndCursor"], one["followerEndCursor"]))
-
 
 def beginReq(currentUser,doViewer,endpoint,followingEndCursor,followerEndCursor):
     print("begin generateGQL---------------------:{0}".format(currentUser))
@@ -410,7 +282,7 @@ def beginReq(currentUser,doViewer,endpoint,followingEndCursor,followerEndCursor)
         try:
             print(op)
             data = endpoint(op)
-            # print("endpoint.result.data:"+data)
+            print("endpoint.result.data:"+data)
         except Exception as e:
             print("done req {0},op:{1},data:{2}".format(taskQueue.get(),op,json.dumps(data)))
             print(e)
@@ -427,11 +299,11 @@ def beginReq(currentUser,doViewer,endpoint,followingEndCursor,followerEndCursor)
                     if tmp == None:
                         usersColl.insert_one(data.get("data").get("viewer"))
 
-            insertListToUser(usersColl,followersList)
-            insertListToUser(usersColl,followingList)
+            # insertListToUser(usersColl,followersList)
+            # insertListToUser(usersColl,followingList)
 
-            insertData(followerColl, currentUser, followersList)
-            insertData(followingColl, currentUser, followingList)
+            # insertData(followerColl, currentUser, followersList)
+            # insertData(followingColl, currentUser, followingList)
 
             haveNextFollowersPage = data.get("data").get("user").get("followers").get("pageInfo").get("hasNextPage")
             haveNextFollowingPage = data.get("data").get("user").get("following").get("pageInfo").get("hasNextPage")
@@ -441,7 +313,7 @@ def beginReq(currentUser,doViewer,endpoint,followingEndCursor,followerEndCursor)
 
             print("---------------------:{0},{1},{2},{3},{4},{5}".format(currentUser, taskQueue.qsize(),haveNextFollowersPage,currentFollowerEndCursor,haveNextFollowingPage,currentFollowingEndCursor))
 
-            saveOrUpdateProgress(usersColl,haveNextFollowingPage,haveNextFollowersPage, currentUser, currentFollowingEndCursor,currentFollowerEndCursor)
+            # saveOrUpdateProgress(usersColl,haveNextFollowingPage,haveNextFollowersPage, currentUser, currentFollowingEndCursor,currentFollowerEndCursor)
 
             print("haveNextFollowersPage:{0}".format(haveNextFollowersPage))
             print("haveNextFollowingPage:{0}".format(haveNextFollowingPage))
